746df3ebf177ac4932c7c9b59ffbc15a9bd42e90
Filip Cyrus Bober <filip.bober@gmail.com>
2016-05-30 13:36:05 +0200

    Added GameManager stub and two zombie graphic prototypes.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance = null;
+
+    void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        else if (Instance != this)
+            Destroy(gameObject);
+    }
+
+    void OnEnable()
+    {
+        EnemyController.EnemyDown += EnemyDown;
+    }
+
+    void OnDisable()
+    {
+        EnemyController.EnemyDown -= EnemyDown;
+    }
+
+    void EnemyDown(object sneder, EnemyPropertiesEventArgs e)
+    {
+        // UI -> subtract ZombiesLeft
+        
+    }
+
+}
