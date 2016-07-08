6fdabca1933162185f9993a445cb1a680cd394a7
Filip Cyrus Bober <filip.bober@gmail.com>
2016-07-08 20:55:52 +0200

    Added quit button.

---
+++ Assets/Scripts/_tmp/PrototypeHUD.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PrototypeHUD : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnExitButtonClicked()
+    {
+        Application.Quit();
+    }
+}
