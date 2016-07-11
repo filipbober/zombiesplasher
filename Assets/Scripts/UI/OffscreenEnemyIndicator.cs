ac5753625d73fe45f5f6740ffafa8dfe9219595c
Filip Cyrus Bober <filip.bober@gmail.com>
2016-07-11 22:24:51 +0200

    Cleaned up enemy indicator.

--- Assets/Scripts/UI/OffscreenEnemyIndicator.cs
+++ Assets/Scripts/UI/OffscreenEnemyIndicator.cs
@@ -7,6 +7,8 @@ public class OffscreenEnemyIndicator : MonoBehaviour
     [SerializeField]
     private GameObject _enemyIndicator;
 
+    private ObjectPooler _enemyIndicatorPool;
+
     private List<GameObject> _enemyList;
 
     private GameObject _tmpInstatiated;
@@ -14,6 +16,8 @@ public class OffscreenEnemyIndicator : MonoBehaviour
     void Awake()
     {
         _enemyList = new List<GameObject>();
+
+        //_enemyIndicatorPool = gameObject.AddComponent(ObjectPooler);
     }
 
     void OnEnable()
@@ -30,6 +34,8 @@ public class OffscreenEnemyIndicator : MonoBehaviour
     {
         foreach (GameObject go in _enemyList)
         {
+            if (!go.activeInHierarchy) return;
+
             Vector3 screenPos = Camera.main.WorldToScreenPoint(go.transform.position);
             bool isOffscreen = !(screenPos.z > 0
                 && screenPos.x > 0 && screenPos.x < Screen.width
@@ -43,6 +49,7 @@ public class OffscreenEnemyIndicator : MonoBehaviour
                 if (_tmpInstatiated == null)
                 {
                     enemyIndicator = Instantiate(_enemyIndicator);
+                    //GameObject enemyIndicator = _enemyIndicatorPool.GetPooledObject();
                     enemyIndicator.transform.SetParent(gameObject.transform, false);    // false to scale image with Canvas
                     _tmpInstatiated = enemyIndicator;
                 }
@@ -51,58 +58,14 @@ public class OffscreenEnemyIndicator : MonoBehaviour
                     enemyIndicator = _tmpInstatiated;
                 }
 
-                Image enemyIndicatorImage = enemyIndicator.GetComponent<Image>();
-
+                Image enemyIndicatorImage = enemyIndicator.GetComponent<Image>();       // TODO: Make list with pooled objects
                 Vector3 viewPos = Camera.main.WorldToViewportPoint(go.transform.position);
-                //Debug.Log(viewPos);
                 Vector2 indicatorPos = new Vector2(Mathf.Clamp(viewPos.x, 0f, 1f), Mathf.Clamp(viewPos.y, 0f, 1f));
 
-                //enemyIndicatorImage.rectTransform.anchoredPosition = new Vector2(viewPos.x, viewPos.y);
-                //enemyIndicatorImage.rectTransform.anchoredPosition = indicatorPos;
-                //enemyIndicatorImage.rectTransform.position = indicatorPos;
-                //enemyIndicatorImage.rectTransform.anchoredPosition = new Vector2(0.9f, 0.9f);
-                //enemyIndicator.transform.position = new Vector2(0.9f, 0.9f);
-
-
                 CanvasScaler scaler = GetComponentInParent<CanvasScaler>();
-                //enemyIndicatorImage.rectTransform.anchoredPosition = new Vector2(Input.mousePosition.x * scaler.referenceResolution.x / Screen.width, Input.mousePosition.y * scaler.referenceResolution.y / Screen.height);
-                //enemyIndicatorImage.rectTransform.anchoredPosition = new Vector2(indicatorPos.x * scaler.referenceResolution.x / Screen.width, indicatorPos.y * scaler.referenceResolution.y / Screen.height);
-                //enemyIndicatorImage.rectTransform.anchoredPosition = new Vector2(0.1f * scaler.referenceResolution.x, 0.1f);
-
-                // ----
-                //if (screenPos.x < 0)
-                //{
-                //    indicatorPos.x = -Screen.width / 2;
-                //}
-                //else if (screenPos.x > Screen.width)
-                //{
-                //    indicatorPos.x = Screen.width / 2;
-                //}
-
-                //if (screenPos.y < 0)
-                //{
-                //    indicatorPos.y = -Screen.height / 2;
-                //}
-                //else if (screenPos.y > Screen.height)
-                //{
-                //    indicatorPos.y = Screen.height / 2;
-                //}
-
-                //enemyIndicatorImage.rectTransform.anchoredPosition = new Vector2(indicatorPos.x, indicatorPos.y);
-                // ----
-
-                // <---
-                //enemyIndicatorImage.rectTransform.anchoredPosition = new Vector2(indicatorPos.x * Screen.width - Screen.width/2, indicatorPos.y * Screen.height - Screen.height/2);
-
-                //enemyIndicatorImage.rectTransform.anchoredPosition = new Vector2(indicatorPos.x * (scaler.referenceResolution.x / Screen.width), indicatorPos.y * Screen.height - Screen.height / 2);
-                //enemyIndicatorImage.rectTransform.anchoredPosition = new Vector2(scaler.referenceResolution.x / Screen.width, scaler.referenceResolution.y / Screen.height);
-
 
                 float scaleX = scaler.referenceResolution.x / Screen.width;
                 float scaleY = scaler.referenceResolution.y / Screen.height;
-                //enemyIndicatorImage.rectTransform.anchoredPosition = new Vector2(indicatorPos.x * (res) * Screen.width - Screen.width * res / 2, indicatorPos.y * Screen.height - Screen.height / 2);                
-                //enemyIndicatorImage.rectTransform.anchoredPosition = new Vector2((indicatorPos.x * Screen.width - Screen.width / 2) * scaleX, (indicatorPos.y * Screen.height - Screen.height / 2) * scaleY);
-
 
                 Vector2 imgSizeModifier = new Vector2(0f, 0f);
                 if (screenPos.x < 0)
@@ -116,33 +79,27 @@ public class OffscreenEnemyIndicator : MonoBehaviour
 
                 if (screenPos.y < 0)
                 {
-                    imgSizeModifier.y += enemyIndicatorImage.rectTransform.rect.height / 2;                    
+                    imgSizeModifier.y += enemyIndicatorImage.rectTransform.rect.height / 2;
                 }
                 else if (screenPos.y > Screen.height)
                 {
                     imgSizeModifier.y -= enemyIndicatorImage.rectTransform.rect.height / 2;
                 }
-                //imgSizeModifier.x *= enemyIndicatorImage.rectTransform.localScale.x;
-                //imgSizeModifier.x /= scaleX;
-                //imgSizeModifier.y /= scaleY;
-                //imgSizeModifier *= 2;
 
-                Debug.Log(imgSizeModifier);
                 Vector2 borderPosition = new Vector2((indicatorPos.x * Screen.width - Screen.width / 2) * scaleX, (indicatorPos.y * Screen.height - Screen.height / 2) * scaleY);
                 borderPosition += imgSizeModifier;
                 enemyIndicatorImage.rectTransform.anchoredPosition = borderPosition;
 
-
-                //Debug.Log("indicatorPos.x = " + indicatorPos.x + ", Screen.width = " + Screen.width);
-                //Debug.Log(scaler.referenceResolution.x);
-
-                return;
+                //return;
             }
         }
     }
 
     void AddEnemyToList(object sender, EnemyPropertiesEventArgs e)
     {
-        _enemyList.Add(e.EnemyGameObj);
+        if (!_enemyList.Contains(e.EnemyGameObj))
+        {
+            _enemyList.Add(e.EnemyGameObj);
+        }
     }
 }
