2dda2e92f0d7a1f8fdffd03031454ad67dcf16a6
Filip Cyrus Bober <filip.bober@gmail.com>
2016-07-11 22:06:04 +0200

    Fixed bugs with enemy indicator.

--- Assets/Scripts/UI/OffscreenEnemyIndicator.cs
+++ Assets/Scripts/UI/OffscreenEnemyIndicator.cs
@@ -31,10 +31,11 @@ public class OffscreenEnemyIndicator : MonoBehaviour
         foreach (GameObject go in _enemyList)
         {
             Vector3 screenPos = Camera.main.WorldToScreenPoint(go.transform.position);
-
-            if (!(screenPos.z > 0
+            bool isOffscreen = !(screenPos.z > 0
                 && screenPos.x > 0 && screenPos.x < Screen.width
-                && screenPos.y > 0 && screenPos.y < Screen.height))
+                && screenPos.y > 0 && screenPos.y < Screen.height);
+
+            if (isOffscreen)
             {
                 Vector3 screenCenter = new Vector3(Screen.width / 2, Screen.height / 2);
 
@@ -42,7 +43,7 @@ public class OffscreenEnemyIndicator : MonoBehaviour
                 if (_tmpInstatiated == null)
                 {
                     enemyIndicator = Instantiate(_enemyIndicator);
-                    enemyIndicator.transform.SetParent(gameObject.transform);
+                    enemyIndicator.transform.SetParent(gameObject.transform, false);    // false to scale image with Canvas
                     _tmpInstatiated = enemyIndicator;
                 }
                 else
@@ -53,7 +54,7 @@ public class OffscreenEnemyIndicator : MonoBehaviour
                 Image enemyIndicatorImage = enemyIndicator.GetComponent<Image>();
 
                 Vector3 viewPos = Camera.main.WorldToViewportPoint(go.transform.position);
-                Debug.Log(viewPos);
+                //Debug.Log(viewPos);
                 Vector2 indicatorPos = new Vector2(Mathf.Clamp(viewPos.x, 0f, 1f), Mathf.Clamp(viewPos.y, 0f, 1f));
 
                 //enemyIndicatorImage.rectTransform.anchoredPosition = new Vector2(viewPos.x, viewPos.y);
@@ -90,8 +91,50 @@ public class OffscreenEnemyIndicator : MonoBehaviour
                 //enemyIndicatorImage.rectTransform.anchoredPosition = new Vector2(indicatorPos.x, indicatorPos.y);
                 // ----
 
-                enemyIndicatorImage.rectTransform.anchoredPosition = new Vector2(indicatorPos.x * Screen.width - Screen.width/2, indicatorPos.y * Screen.height - Screen.height/2);
+                // <---
+                //enemyIndicatorImage.rectTransform.anchoredPosition = new Vector2(indicatorPos.x * Screen.width - Screen.width/2, indicatorPos.y * Screen.height - Screen.height/2);
 
+                //enemyIndicatorImage.rectTransform.anchoredPosition = new Vector2(indicatorPos.x * (scaler.referenceResolution.x / Screen.width), indicatorPos.y * Screen.height - Screen.height / 2);
+                //enemyIndicatorImage.rectTransform.anchoredPosition = new Vector2(scaler.referenceResolution.x / Screen.width, scaler.referenceResolution.y / Screen.height);
+
+
+                float scaleX = scaler.referenceResolution.x / Screen.width;
+                float scaleY = scaler.referenceResolution.y / Screen.height;
+                //enemyIndicatorImage.rectTransform.anchoredPosition = new Vector2(indicatorPos.x * (res) * Screen.width - Screen.width * res / 2, indicatorPos.y * Screen.height - Screen.height / 2);                
+                //enemyIndicatorImage.rectTransform.anchoredPosition = new Vector2((indicatorPos.x * Screen.width - Screen.width / 2) * scaleX, (indicatorPos.y * Screen.height - Screen.height / 2) * scaleY);
+
+
+                Vector2 imgSizeModifier = new Vector2(0f, 0f);
+                if (screenPos.x < 0)
+                {
+                    imgSizeModifier.x += enemyIndicatorImage.rectTransform.rect.width / 2;
+                }
+                else if (screenPos.x > Screen.width)
+                {
+                    imgSizeModifier.x -= enemyIndicatorImage.rectTransform.rect.width / 2;
+                }
+
+                if (screenPos.y < 0)
+                {
+                    imgSizeModifier.y += enemyIndicatorImage.rectTransform.rect.height / 2;                    
+                }
+                else if (screenPos.y > Screen.height)
+                {
+                    imgSizeModifier.y -= enemyIndicatorImage.rectTransform.rect.height / 2;
+                }
+                //imgSizeModifier.x *= enemyIndicatorImage.rectTransform.localScale.x;
+                //imgSizeModifier.x /= scaleX;
+                //imgSizeModifier.y /= scaleY;
+                //imgSizeModifier *= 2;
+
+                Debug.Log(imgSizeModifier);
+                Vector2 borderPosition = new Vector2((indicatorPos.x * Screen.width - Screen.width / 2) * scaleX, (indicatorPos.y * Screen.height - Screen.height / 2) * scaleY);
+                borderPosition += imgSizeModifier;
+                enemyIndicatorImage.rectTransform.anchoredPosition = borderPosition;
+
+
+                //Debug.Log("indicatorPos.x = " + indicatorPos.x + ", Screen.width = " + Screen.width);
+                //Debug.Log(scaler.referenceResolution.x);
 
                 return;
             }
