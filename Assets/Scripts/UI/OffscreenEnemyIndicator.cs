c73fb3e2916459e8c401581bf7cb05ec6af78e2b
Filip Cyrus Bober <filip.bober@gmail.com>
2016-07-12 17:19:09 +0200

    EnemyIndicator math improved, but still some errors.

--- Assets/Scripts/UI/OffscreenEnemyIndicator.cs
+++ Assets/Scripts/UI/OffscreenEnemyIndicator.cs
@@ -71,10 +71,17 @@ public class OffscreenEnemyIndicator : MonoBehaviour
                 Vector3 viewPos = Camera.main.WorldToViewportPoint(go.transform.position);
                 Vector2 indicatorPos = new Vector2(Mathf.Clamp(viewPos.x, 0f, 1f), Mathf.Clamp(viewPos.y, 0f, 1f));
 
+                // Subtract, because center is in 0.5 and we want adjust everything to the left-down corner
+                indicatorPos.x -= 0.5f;
+                indicatorPos.y -= 0.5f;
+                //Vector2 indicatorPos = new Vector2(Mathf.Clamp(viewPos.x, -1f, 1f), Mathf.Clamp(viewPos.y, -1f, 1f));
+
                 CanvasScaler scaler = GetComponentInParent<CanvasScaler>();
 
                 float scaleX = scaler.referenceResolution.x / Screen.width;
-                float scaleY = scaler.referenceResolution.y / Screen.height;               
+                float scaleY = scaler.referenceResolution.y / Screen.height;
+
+                scaleY = scaleX;        // Scaler matches width, so only scale in X matters            
 
                Vector2 imgSizeModifier = new Vector2(0f, 0f);
                 if (screenPos.x < 0)
@@ -94,13 +101,24 @@ public class OffscreenEnemyIndicator : MonoBehaviour
                 {
                     imgSizeModifier.y -= enemyIndicatorImage.rectTransform.rect.height / 2;
                 }
-                
-                imgSizeModifier.x *= scaleX;
-                imgSizeModifier.y *= scaleY;
 
-                Vector2 borderPosition = new Vector2((indicatorPos.x * Screen.width - Screen.width / 2) * scaleX, (indicatorPos.y * Screen.height - Screen.height / 2) * scaleY);
-                borderPosition += imgSizeModifier;
+                //imgSizeModifier.x *= scaleX;
+                //imgSizeModifier.y *= scaleY;
+
+                // Correct
+                //Vector2 borderPosition = new Vector2((indicatorPos.x * Screen.width - Screen.width / 2) * scaleX, (indicatorPos.y * Screen.height - Screen.height / 2) * scaleY);
+
+                // In Progress
+                //Vector2 borderPosition = new Vector2((indicatorPos.x * Screen.width * scaleX * 0.5f - (Screen.width / 2 * scaleX)) , (indicatorPos.y * Screen.height * scaleY * 0.5f) );
+                Vector2 borderPosition = new Vector2((indicatorPos.x * Screen.width * scaleX), (indicatorPos.y * Screen.height * scaleY));
+                borderPosition += imgSizeModifier;      
                 enemyIndicatorImage.rectTransform.anchoredPosition = borderPosition;
+
+                //Debug.Log("Screen.width = " + Screen.width);
+                //Debug.Log("scaleX = " + scaleX);
+                //Debug.Log("image width = " + enemyIndicatorImage.rectTransform.rect.width);
+                //Debug.Log("indicatorPos.x = " + indicatorPos.x);
+                //Debug.Log("---------------------------------------");
 
                 return;
             }
