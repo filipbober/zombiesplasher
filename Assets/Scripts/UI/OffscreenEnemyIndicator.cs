64f9f301091b3ef689e0bdac0a213158ecfe9880
Filip Cyrus Bober <filip.bober@gmail.com>
2016-07-13 13:06:42 +0200

    Indicator image is now modified by image offset.

--- Assets/Scripts/UI/OffscreenEnemyIndicator.cs
+++ Assets/Scripts/UI/OffscreenEnemyIndicator.cs
@@ -54,13 +54,11 @@ public class OffscreenEnemyIndicator : MonoBehaviour
                 GameObject enemyIndicator;
                 if (_tmpInstatiated == null)
                 {
-                    //enemyIndicator = Instantiate(_enemyIndicator);
+                    ;
                     enemyIndicator = _enemyIndicatorPool.GetPooledObject();
                     enemyIndicator.SetActive(true);
 
-                    //enemyIndicator.transform.SetParent(gameObject.transform, false);    // false to scale image with Canvas
                     enemyIndicator.transform.SetParent(_hudCavas.transform, false);     // false to scale image with Canvas
-                    //enemyIndicator.transform.SetParent(gameObject.transform, true);
 
                     _tmpInstatiated = enemyIndicator;
                 }
@@ -71,57 +69,11 @@ public class OffscreenEnemyIndicator : MonoBehaviour
 
                 Image enemyIndicatorImage = enemyIndicator.GetComponent<Image>();       // TODO: Make list with pooled objects
                 Vector3 viewPos = Camera.main.WorldToViewportPoint(go.transform.position);
-                Vector2 indicatorPos = new Vector2(Mathf.Clamp(viewPos.x, 0f, 1f), Mathf.Clamp(viewPos.y, 0f, 1f));
-
-                // Subtract, because center is in 0.5 and we want adjust everything to the left-down corner
-                indicatorPos.x -= 0.5f;
-                indicatorPos.y -= 0.5f;
-                //Vector2 indicatorPos = new Vector2(Mathf.Clamp(viewPos.x, -1f, 1f), Mathf.Clamp(viewPos.y, -1f, 1f));
 
                 CanvasScaler scaler = GetComponentInParent<CanvasScaler>();
-
-                Debug.Log("viewPos = " + viewPos);
-                Debug.Log("Screen.height = " + Screen.height);
-                Debug.Log(Camera.main.ViewportToScreenPoint(viewPos));
-
                 float scaleX = scaler.referenceResolution.x / Screen.width;
-                float scaleY = scaler.referenceResolution.y / Screen.height;
-
-                scaleY = scaleX;        // Scaler matches width, so only scale in X matters            
-
-               Vector2 imgSizeModifier = new Vector2(0f, 0f);
-                if (screenPos.x < 0)
-                {
-                    imgSizeModifier.x += enemyIndicatorImage.rectTransform.rect.width / 2;
-                }
-                else if (screenPos.x > Screen.width)
-                {
-                    imgSizeModifier.x -= enemyIndicatorImage.rectTransform.rect.width / 2;
-                }
-
-                if (screenPos.y < 0)
-                {
-                    imgSizeModifier.y += enemyIndicatorImage.rectTransform.rect.height / 2;
-                }
-                else if (screenPos.y > Screen.height)
-                {
-                    imgSizeModifier.y -= enemyIndicatorImage.rectTransform.rect.height / 2;
-                }
-
-                //imgSizeModifier.x *= scaleX;
-                //imgSizeModifier.y *= scaleY;
-
-                // Correct
-                //Vector2 borderPosition = new Vector2((indicatorPos.x * Screen.width - Screen.width / 2) * scaleX, (indicatorPos.y * Screen.height - Screen.height / 2) * scaleY);
-
-                // In Progress
-                //Vector2 borderPosition = new Vector2((indicatorPos.x * Screen.width * scaleX * 0.5f - (Screen.width / 2 * scaleX)) , (indicatorPos.y * Screen.height * scaleY * 0.5f) );
-
-                //Vector2 borderPosition = new Vector2((indicatorPos.x * Screen.width * scaleX), (indicatorPos.y * Screen.height * scaleY));
-                //borderPosition += imgSizeModifier;      
-                //enemyIndicatorImage.rectTransform.anchoredPosition = borderPosition;
-
-
+                //float scaleY = scaler.referenceResolution.y / Screen.height;
+                float scaleY = scaleX;      // Scaler matches width, so only scale in X matters               
 
                 Vector3 screenPoint = Camera.main.ViewportToScreenPoint(viewPos);
                 Vector2 borderPosition = new Vector3(Mathf.Clamp(screenPoint.x, 0f, Screen.width), Mathf.Clamp(screenPoint.y, 0f, Screen.height), 0f);
@@ -132,14 +84,36 @@ public class OffscreenEnemyIndicator : MonoBehaviour
                 borderPosition.y -= Screen.height / 2;
                 borderPosition.y *= scaleY;
 
+                // Adjust position for the image to fit the screen
+                Vector2 imageOffset;
+                Vector2 imageSize = new Vector2(enemyIndicatorImage.rectTransform.rect.width, enemyIndicatorImage.rectTransform.rect.height);
+
+                int offsetSignX = 0;
+                int offsetSignY = 0;
+                if (screenPos.x < 0)
+                {
+                    offsetSignX = 1;
+                }
+                else if (screenPos.x > Screen.width)
+                {
+                    offsetSignX = -1;
+                }
+
+                if (screenPos.y < 0)
+                {
+                    offsetSignY = 1;
+                }
+                else if (screenPos.y > Screen.height)
+                {
+                    offsetSignY = -1;
+                }
+
+                imageOffset = new Vector2(imageSize.x / 2, imageSize.y / 2);
+                imageOffset.x *= offsetSignX;
+                imageOffset.y *= offsetSignY;
+                borderPosition += imageOffset;
+
                 enemyIndicatorImage.rectTransform.anchoredPosition = borderPosition;
-
-
-                //Debug.Log("Screen.width = " + Screen.width);
-                //Debug.Log("scaleX = " + scaleX);
-                //Debug.Log("image width = " + enemyIndicatorImage.rectTransform.rect.width);
-                //Debug.Log("indicatorPos.x = " + indicatorPos.x);
-                //Debug.Log("---------------------------------------");
 
                 return;
             }
