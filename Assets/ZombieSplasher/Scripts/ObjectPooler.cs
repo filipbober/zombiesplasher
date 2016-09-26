f35852616e7d36e10950cb133483b93fd5333769
Filip Cyrus Bober <filip.bober@gmail.com>
2016-09-26 22:02:07 +0200

    Reorganized folder and scene structure

---
+++ Assets/ZombieSplasher/Scripts/ObjectPooler.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class ObjectPooler : MonoBehaviour
+{
+
+    public static ObjectPooler CurrentInstance;
+
+    [SerializeField]
+    private GameObject _pooledObject;
+
+    [SerializeField]
+    private int _pooledAmount = 20;
+
+    [SerializeField]
+    private bool _willGrow = true;
+
+
+    private List<GameObject> _pooledObjects;
+
+    //---------------------------------------------------------------------
+    // Public methods
+    //--------------------------------------------------------------------- 
+    public GameObject GetPooledObject()
+    {
+        // Find inactive pool object and return it. 
+        // Setting that object to active is not the responsibility of this method.
+        for (int i = 0; i < _pooledObjects.Count; i++)
+        {
+            if (!_pooledObjects[i].activeInHierarchy)
+            {
+                return _pooledObjects[i];
+            }
+        }
+
+        // Expand the collection if it's too small and growing is permitted.
+        if (_willGrow)
+        {
+            GameObject obj = (GameObject)Instantiate(_pooledObject);
+            obj.transform.SetParent(transform);
+            _pooledObjects.Add(obj);
+            return obj;
+        }
+
+        // We are out of object and we are not allowed to create new ones.
+        return null;
+
+    }
+
+    //---------------------------------------------------------------------
+    // Mono protected methods
+    //--------------------------------------------------------------------- 
+    protected void Awake()
+    {
+        CurrentInstance = this;
+    }
+
+    protected void Start()
+    {
+        // Create a pool of objects.
+        _pooledObjects = new List<GameObject>();
+        for (int i = 0; i < _pooledAmount; i++)
+        {
+            GameObject obj = (GameObject)Instantiate(_pooledObject);
+            obj.transform.SetParent(transform);
+            _pooledObjects.Add(obj);
+            obj.SetActive(false);
+
+        }
+    }
+}
---
+++ Assets/ZombieSplasher/Scripts/UI/GameHUD.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameHUD : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnExitButtonClicked()
+    {
+        Application.Quit();
+    }
+
+    public void OnPlay()
+    {
+        MainController.SwitchScene(Scenes.TestScene);
+    }
+}
