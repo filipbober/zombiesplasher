21a00b182b8a8b08774d2686604db07bf1a25e4d
Filip Cyrus Bober <filip.bober@gmail.com>
2016-10-05 21:47:17 +0200

    Add concrete EventHandler

---
+++ Assets/EventSystem/EventHandler.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+namespace EventSystem
+{
+    public abstract class EventHandler : MonoBehaviour, IEventHandler
+    {
+        public abstract void SubscribeEvents();
+        public abstract void UnsubscribeEvents();
+
+        protected virtual void OnEnable()
+        {
+            SubscribeEvents();
+        }
+
+        protected virtual void OnDisable()
+        {
+            UnsubscribeEvents();
+        }
+    }
+}
