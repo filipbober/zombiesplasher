29c89df2f9156d699d61178f9b0fcae3484c4dfa
Filip Cyrus Bober <filip.bober@gmail.com>
2016-11-16 22:22:40 +0100

    Add RenderWithShader test

---
+++ Assets/ZombieSplasher/BackgroundRender/Scripts/DynamicObjectShader.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class DynamicObjectShader : MonoBehaviour
+{
+    [SerializeField]
+    private Shader _shader;
+
+    protected void Awake()
+    {
+        if (_shader)
+            GetComponent<Camera>().SetReplacementShader(_shader, null);
+    }
+
+}
